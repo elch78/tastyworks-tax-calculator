6afbe3523df2ae18a8939b0316268caab50b6237
Nils Rommelfanger <nils.rommelfanger@beyonnex.io>
2023-03-10 17:14:15 +0100

    refactor: remove unused methods

--- src/main/kotlin/com/elchworks/tastyworkstaxcalculator/positions/ProfitAndLoss.kt
+++ src/main/kotlin/com/elchworks/tastyworkstaxcalculator/positions/ProfitAndLoss.kt
@@ -5,9 +5,6 @@ data class ProfitAndLoss(
     val loss: Float,
 ) {
     constructor(): this(0.0f, 0.0f)
-
-    fun addProfit(value: Float) = ProfitAndLoss(profit + value, loss)
-    fun addLoss(value: Float) = ProfitAndLoss(profit, loss + value)
 }
 
 operator fun ProfitAndLoss.plus(other: ProfitAndLoss) = ProfitAndLoss(profit + other.profit, loss + other.loss)
