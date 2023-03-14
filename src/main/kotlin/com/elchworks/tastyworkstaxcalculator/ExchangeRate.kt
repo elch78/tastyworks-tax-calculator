fd2280851e659e2b32824cfe7b8845721ace8d6f
Nils Rommelfanger <nils.rommelfanger@beyonnex.io>
2023-03-14 11:09:33 +0100

    fix: add exchange rates for 2022

--- src/main/kotlin/com/elchworks/tastyworkstaxcalculator/ExchangeRate.kt
+++ src/main/kotlin/com/elchworks/tastyworkstaxcalculator/ExchangeRate.kt
@@ -14,6 +14,20 @@ class ExchangeRate {
 
     // source: https://de.statista.com/statistik/daten/studie/214878/umfrage/wechselkurs-des-euro-gegenueber-dem-us-dollar-monatliche-entwicklung/
     private val rates = mapOf(
+        "2023-2" to 1.07F,
+        "2023-1" to 1.08F,
+        "2022-12" to 1.06F,
+        "2022-11" to 1.02F,
+        "2022-10" to 0.98F,
+        "2022-9" to 0.99F,
+        "2022-8" to 1.01F,
+        "2022-7" to 1.02F,
+        "2022-6" to 1.06F,
+        "2022-5" to 1.06F,
+        "2022-4" to 1.08F,
+        "2022-3" to 1.1F,
+        "2022-2" to 1.13F,
+        "2022-1" to 1.13F,
         "2021-12" to 1.13F,
         "2021-11" to 1.14F,
     )
