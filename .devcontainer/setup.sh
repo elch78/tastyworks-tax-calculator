62dd0818803d48f61772939c4d5682a2cedf0420
Nils Rommelfanger <nelhimoud@gmail.com>
2025-11-03 10:53:05 +0000

    iterate on structurizr

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,10 +3,6 @@ set -e
 
 WORKSPACE_DIR="${1:-$(pwd)}"
 
-echo "Installing system packages..."
-sudo apt-get update
-sudo apt-get install -y graphviz
-
 echo "Configuring git..."
 git config --global --add safe.directory "$WORKSPACE_DIR"
 
@@ -19,11 +15,6 @@ npm install -g @anthropic-ai/claude-code
 
 echo "Setting up local bin directory..."
 mkdir -p /home/developer/.local/bin
-
-#echo "Downloading PlantUML..."
-#wget -q -O /home/developer/.local/bin/plantuml.jar \
-#  https://github.com/plantuml/plantuml/releases/download/v1.2024.7/plantuml.jar
-#echo 'alias plantuml="java -jar /home/developer/.local/bin/plantuml.jar"' >> /home/developer/.bashrc
 
 echo "Downloading Structurizr Lite..."
 wget -q -O /home/developer/.local/bin/structurizr-lite.war \
